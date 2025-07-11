c7d2a19c6eeb83673682027f2a6ad1d48cdc2024
chaeyn <chaulfe@gmail.com>
2025-07-11 20:08:19 +0900

    fix: change Admin page text

--- apps/admin/src/App.tsx
+++ apps/admin/src/App.tsx
@@ -2,10 +2,10 @@ import { Button } from '@sh4re/ui';
 
 function App() {
   return (
-      <div>
-        <h1>Student</h1>
-        <Button>Shared UI</Button>
-      </div>
+    <div>
+      <h1>Admin</h1>
+      <Button>Shared UI</Button>
+    </div>
   );
 }
 
