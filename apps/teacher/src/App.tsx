34574f245bf7adb180818253a0eb9a3dd8058d6b
Gorani <baconbacon1231@gmail.com>
2025-07-11 19:02:12 +0900

    build: 모노 레포 및 스토리북 세팅

---
+++ apps/teacher/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@sh4re/ui';
+
+function App() {
+  return (
+      <div>
+        <h1>Teacher</h1>
+        <Button>Shared UI</Button>
+      </div>
+  );
+}
+
+export default App;
