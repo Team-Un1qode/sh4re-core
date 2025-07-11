d988a7724af41fc12fb252efb9d4d509fb67ea46
chaeyn <chaulfe@gmail.com>
2025-07-11 19:40:37 +0900

    build: add admin page

---
+++ apps/admin/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@sh4re/ui';
+
+function App() {
+  return (
+      <div>
+        <h1>Student</h1>
+        <Button>Shared UI</Button>
+      </div>
+  );
+}
+
+export default App;
