d1499e7db2426ad56691c3f641e3d8878202ed56
Gorani <baconbacon1231@gmail.com>
2025-07-11 19:23:26 +0900

    build: 빌드 설정 수정

--- packages/ui/src/components/Button.tsx
+++ packages/ui/src/components/Button.tsx
@@ -4,7 +4,7 @@ export interface ButtonProps {
   children: React.ReactNode;
 }
 
+
 export const Button = ({ children }: ButtonProps) => {
   return <button style={{ padding: '10px' }}>{children}</button>;
 };
-
